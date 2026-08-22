eee00a758755a04f3989e4960c4f257897aafa67
shrey4sh <shrey4sh@users.noreply.github.com>
2026-08-22 19:14:51 +0000

    fix: setValue delegate import

--- app/src/main/java/com/shrey4sh/rabbithole/ui/history/HistoryScreen.kt
+++ app/src/main/java/com/shrey4sh/rabbithole/ui/history/HistoryScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
